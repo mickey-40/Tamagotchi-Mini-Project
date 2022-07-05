57f5afb9d96067b9f608a5ab78e97cda87601267
mickey-40 <mickeyarnold53@yahoo.com>
2022-07-05 18:49:13 -0500

    added class of pet and user input of name

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log("hello")
+//Require pet class
+
+class Pet {
+  constructor(name, hunger, sleepniness, boredom, age) {
+    this.name = name
+    this.hunger = hunger
+    this.sleepniness = sleepniness
+    this.boredom = boredom
+    this.age = age
+  }
+}
+
+//Require game object or class
+class game {
+
+}
+
+const myPet = new Pet()
+myPet.name = window.prompt("Enter your name: ")
+console.log(myPet.name)
