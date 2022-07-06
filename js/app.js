f1879efcfa0c46241d1c5e12ca1dc5035a9902cd
mickey-40 <mickeyarnold53@yahoo.com>
2022-07-05 21:55:16 -0500

    set increment and background image

--- js/app.js
+++ js/app.js
@@ -9,16 +9,36 @@ class Pet {
     this.age = age
   }
   //function that rises Attributes
-  raiseAttributes = () => {
-    myPet.age += 1
-    document.querySelector('.age').innerText = `Age : ${myPet.age}`
-    myPet.hunger += 1
-    document.querySelector('.hunger').innerText = `Hunger : ${myPet.hunger}`
-    myPet.sleepniness += 1
-    document.querySelector('.sleepiness').innerText = `Sleepiness : ${myPet.sleepniness}`
-    myPet.boredom += 1
-    document.querySelector('.boredom').innerText = `Boredom : ${myPet.boredom}`
+  ageUp = () => {
+    this.age += 1
+    document.querySelector('.age').innerText = `Age : ${this.age}`
   }
+  hungerUp = () => {
+    if (this.hunger < 10){
+      this.hunger += 1
+      document.querySelector('.hunger').innerText = `Hunger : ${this.hunger}`    
+    } else {
+      return alert('dead')
+    }
+  }
+  sleepUp = () => {
+    if (this.sleepniness < 10){
+      this.sleepniness += 1
+      document.querySelector('.sleepniness').innerText = `Sleepniness : ${this.sleepniness}`    
+    } else {
+      return alert('dead')
+    }
+  }
+  boredomUp = () => {
+    if (this.boredom < 10){
+      this.boredom += 1
+      document.querySelector('.boredom').innerText = `Boredom : ${this.boredom}`    
+    } else {
+      return alert('dead')
+    }
+    
+  }
+
 }
 
 //Require game object or class
@@ -30,7 +50,10 @@ class game {
 const myPet = new Pet()
 document.querySelector('#start').addEventListener('click', function(){
   myPet.name = window.prompt("Enter your name: ")
-  setInterval(myPet.riseAttributes, 2000)
+  setInterval(myPet.ageUp, 1000)
+  setInterval(myPet.hungerUp, 2000)
+  setInterval(myPet.sleepUp, 5000)
+  setInterval(myPet.boredomUp, 3000)
 })
 
 document.querySelector('#feed').addEventListener('click', function(){
