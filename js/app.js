87d97d493622ebb3c64c27609e27042e62c3b7de
mickey-40 <mickeyarnold53@yahoo.com>
2022-07-07 11:14:27 -0500

    the js functionality is working execpt the animation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //Require pet class
-
 class Pet {
   constructor(name, hunger = 0, sleepniness = 0, boredom = 0, age = 0) {
     this.name = name
@@ -21,10 +20,8 @@ class Pet {
       alert('Your puppy is now a dog!')
     }
     document.querySelector('.age').innerText = `Age : ${this.age}`
-    
-
   }
-//function to increment hunger by 1 and stop the interval when reaches 10
+//function to increment hunger by 1 and stop the interval when reaches 10 or greater
   hungerUp = () => {    
     if (this.hunger >= 10) {
       stopInterval()
@@ -35,10 +32,10 @@ class Pet {
       
     }
   }
-
+//function to increment sleepniness by 1 and stop the interval when reaches 10 or greater
   sleepUp = () => {
     
-    if (this.sleepniness === 10){
+    if (this.sleepniness >= 10){
       stopInterval()
       return alert('fainted of sleepniness')
     } else {
@@ -46,7 +43,7 @@ class Pet {
       document.querySelector('.sleepniness').innerText = `Sleepniness : ${this.sleepniness}` 
     }
   }
-  
+  //function to increment boredom by 1 and stop the interval when reaches 10 or greater
   boredomUp = () => {
     if (this.boredom >= 10){
       stopInterval()
@@ -60,21 +57,17 @@ class Pet {
   }
 
 }
-
-//Require game object or class
-
-
-
+//Instantiate my element to the class
 const myPet = new Pet()
+//event listener to start the game
 document.querySelector('#start').addEventListener('click', function(){
+  //User enters the name of the pet.
   myPet.name = window.prompt("Enter your name: ")
+  //Replaces the text in the h1 with the user input
   document.querySelector('h1').innerText = myPet.name
-
+//starts incrementing all the metrics
   startInterval()
-  // setInterval(myPet.ageUp, 1000)
-  // startHungerInterval()
-  // setInterval(myPet.sleepUp, 2000)
-  // setInterval(myPet.boredomUp, 3000)
+ 
 })
 //set element names to enable the ablity to start and stop the interval
 let hungerinterval;
@@ -96,28 +89,20 @@ const stopInterval = () => {
   clearInterval(boredomInterval)
 
 }
-// let hun;
-// const startHungerInterval = () => {
-//   hun = setInterval(myPet.hungerUp, 1000)
-// }
-// const stopHungerInterval = () => {
-//   clearInterval(hun)
-// }
-
+//Event listener when "feed" button is clicked it reduces hunger metric by one.
 document.querySelector('#feed').addEventListener('click', function(){
   myPet.hunger -= 1
   document.querySelector('.hunger').innerText = `Hunger : ${myPet.hunger}`
-
 })
+//Event listener when "play with pet" button is clicked reduces boredom by one.
 document.querySelector('#play').addEventListener('click', function(){
   myPet.boredom -= 1
   document.querySelector('.boredom').innerText = `Boredom : ${myPet.boredom}`
-  const movement = document.querySelector('#puppyContainer')
-  movement.classList.add('puppyMove')
-  alert('Once a puppy starts moving it does not stop :)')
+  // const movement = document.querySelector('#puppyContainer')
+  // movement.classList.add('puppyMove')
+  // alert('Once a puppy starts moving it does not stop :)')
   })
-
-
+//Event listener when "Turn off the lights" button is clicked reduces boredom by one.
 document.querySelector('#sleep').addEventListener('click', function(){
   myPet.sleepniness -= 1
   document.querySelector('.sleepniness').innerText = `Sleepniness : ${myPet.sleepniness}`
