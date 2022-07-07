ad5754ebb20b3213bf7c3b8e117f90eb49898a4d
mickey-40 <mickeyarnold53@yahoo.com>
2022-07-06 19:23:22 -0500

    js is functioning

--- js/app.js
+++ js/app.js
@@ -1,79 +1,92 @@
-// //Require pet class
+//Require pet class
 
-// class Pet {
-//   constructor(name, hunger = 0, sleepniness = 0, boredom = 0, age = 0) {
-//     this.name = name
-//     this.hunger = hunger
-//     this.sleepniness = sleepniness
-//     this.boredom = boredom
-//     this.age = age
-//   }
-//   //function that rises Attributes
-//   ageUp = () => {
-//     this.age += 1
-//     document.querySelector('.age').innerText = `Age : ${this.age}`
-//   }
-//   hungerUp = () => {
-//     if (this.hunger < 10){
-//       this.hunger += 1
-//       document.querySelector('.hunger').innerText = `Hunger : ${this.hunger}`    
-//     } else {
-//       return alert('dead')
-//     }
-//   }
-//   sleepUp = () => {
-//     if (this.sleepniness < 10){
-//       this.sleepniness += 1
-//       document.querySelector('.sleepniness').innerText = `Sleepniness : ${this.sleepniness}`    
-//     } else {
-//       return alert('dead')
-//     }
-//   }
-//   boredomUp = () => {
-//     if (this.boredom < 10){
-//       this.boredom += 1
-//       document.querySelector('.boredom').innerText = `Boredom : ${this.boredom}`    
-//     } else {
-//       return alert('dead')
-//     }
+class Pet {
+  constructor(name, hunger = 0, sleepniness = 0, boredom = 0, age = 0) {
+    this.name = name
+    this.hunger = hunger
+    this.sleepniness = sleepniness
+    this.boredom = boredom
+    this.age = age
+  }
+  //function that rises Attributes
+  ageUp = () => {
+    this.age += 1
+    let element = document.querySelector('#puppyContainer')
+    let element2 = document.querySelector('#dogContainer')
+    if (this.age > 5){
+      element.classList.add('turnoff')
+      element2.classList.remove('turnoff')
+    }
+    document.querySelector('.age').innerText = `Age : ${this.age}`
+
+  }
+  hungerUp = () => {
+    if (this.hunger < 10){
+      this.hunger += 1
+      document.querySelector('.hunger').innerText = `Hunger : ${this.hunger}`    
+    } else {
+      // return alert('fainted')
+    }
+  }
+  sleepUp = () => {
+    if (this.sleepniness < 10){
+      this.sleepniness += 1
+      document.querySelector('.sleepniness').innerText = `Sleepniness : ${this.sleepniness}`    
+    } else {
+      // return alert('fainted')
+    }
+  }
+  boredomUp = () => {
+    if (this.boredom < 10){
+      this.boredom += 1
+      document.querySelector('.boredom').innerText = `Boredom : ${this.boredom}`    
+    } else {
+      // return alert('fainted')
+    }
     
-//   }
-
-// }
-
-// //Require game object or class
-// class game {
-
-// }
-
-
-// const myPet = new Pet()
-// document.querySelector('#start').addEventListener('click', function(){
-//   myPet.name = window.prompt("Enter your name: ")
-//   setInterval(myPet.ageUp, 1000)
-//   setInterval(myPet.hungerUp, 2000)
-//   setInterval(myPet.sleepUp, 5000)
-//   setInterval(myPet.boredomUp, 3000)
-// })
-
-// document.querySelector('#feed').addEventListener('click', function(){
-//   myPet.hunger -= 1
-//   document.querySelector('.hunger').innerText = `Hunger : ${myPet.hunger}`
-
-// })
-// document.querySelector('#play').addEventListener('click', function(){
-//   myPet.boredom -= 1
-//   document.querySelector('.boredom').innerText = `Boredom : ${myPet.boredom}`
-
-// })
-// document.querySelector('#sleep').addEventListener('click', function(){
-//   myPet.sleepniness -= 1
-//   document.querySelector('.sleepniness').innerText = `Sleepniness : ${myPet.sleepniness}`
-// })
-
-const lightsOff = () => {
-  let element = document.querySelector('#lights')
-  element.classList.toggle('lights-off')
+  }
 
 }
+
+//Require game object or class
+class game {
+
+}
+
+
+const myPet = new Pet()
+document.querySelector('#start').addEventListener('click', function(){
+  myPet.name = window.prompt("Enter your name: ")
+  document.querySelector('h1').innerText = myPet.name
+
+  setInterval(myPet.ageUp, 1000)
+  setInterval(myPet.hungerUp, 2000)
+  setInterval(myPet.sleepUp, 2000)
+  setInterval(myPet.boredomUp, 3000)
+})
+
+document.querySelector('#feed').addEventListener('click', function(){
+  myPet.hunger -= 1
+  document.querySelector('.hunger').innerText = `Hunger : ${myPet.hunger}`
+
+})
+document.querySelector('#play').addEventListener('click', function(){
+  myPet.boredom -= 1
+  document.querySelector('.boredom').innerText = `Boredom : ${myPet.boredom}`
+
+})
+document.querySelector('#sleep').addEventListener('click', function(){
+  myPet.sleepniness -= 1
+  document.querySelector('.sleepniness').innerText = `Sleepniness : ${myPet.sleepniness}`
+
+})
+
+function myStop() {
+  clearInterval(myInterval);
+}
+
+
+
+
+
 
